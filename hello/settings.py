a9fc0c3bbc884a3f26a89b8a48f0dd3a05640b1f
usdatempinversion <tempinversion@gmail.com>
2016-08-24 10:40:37 -0500

    ki

--- hello/settings.py
+++ hello/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'hello.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['app/polls/templates'],
+        'DIRS': ['polls/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
