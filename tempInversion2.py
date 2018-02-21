304118303ff38af9c610a258d27cdcd711b0ba28
usdatempinversion <usdaars.uav@gmail.com>
2018-02-20 21:59:23 -0600

    Update tempInversion2.py

--- tempInversion2.py
+++ tempInversion2.py
@@ -302,7 +302,7 @@ def getHighTempFromCSV(mostRecentTime, data):
 def tempInvFromHTML(data):
     # Check if data is empty
     # If it is, return empty result
-    if (data):
+    if (not data):
         return [True, 0, 0, 0, 0, 0, 0, 0, True]
 
     # Get most recent data
